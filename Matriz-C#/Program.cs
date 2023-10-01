e4e49c0a48dd4d4d6b88ec818468fc32d550ff90
DiegoMerida28 <145516421+DiegoMerida28@users.noreply.github.com>
2023-09-30 22:28:03 -0600

    Agregar archivos de proyecto.

---
+++ Matriz-C#/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    const int maxf = 3;
+    const int maxc = 5;
+
+    static void Main(string[ares])
+    {
+        float[,] a = new float[maxf, maxc];
+        int f, c;
+
+        //Leer el array
+        for (f = 0, f < maxf; f++)
+        {
+            for (c = 0; c < maxc; c++)
+            {
+                a[a, c] = float.Parse(Console.ReadLine());
+            }
+        }
+
+        //Escribir el array
+        for (f = 0, f < maxf; f++)
+        {
+            for (c = 0; c < maxc; c++)
+            {
+                Console.Write(a[f, c] + "");
+            }
+            Console.WriteLine();
+        }
+    }
+}
